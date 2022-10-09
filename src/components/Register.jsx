3068d0d053e5968318174660d1488d40cd6ca76c
Johnnie Hurricane <kroyen@ya.ru>
2022-10-09 14:12:15 +0300

    feat: register block added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Register({ handleRegister }) {
 
-    const [state, setState] = useState({
+    const [state, setState] = React.useState({
         email: "",
         password: "",
     });
